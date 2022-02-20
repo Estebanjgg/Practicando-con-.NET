96956a6c52b9423b59511df1a34707507d3ca9f4
Esteban Jose Gonzalez Gomez <91167097+Estebanjgg@users.noreply.github.com>
2022-02-20 11:08:41 -0300

    criando menu para alunos inserir listar e media

--- Program.cs
+++ Program.cs
@@ -3,4 +3,41 @@ Console.WriteLine("Informe a opção desejada:");
 Console.WriteLine("1- Inserir novo aluno");
 Console.WriteLine("2- Listar alunos");
 Console.WriteLine("3- Calcular média geral");
+Console.WriteLine("X- Sair");
 Console.WriteLine();
+
+string opcaoUsuario = Console.ReadLine();
+
+while (opcaoUsuario.ToUpper() != "X")
+{
+    switch(opcaoUsuario)
+    
+{
+        case "1":
+             //TODO: ADICIONAR ALUNO           
+            break;
+
+        case "2":  
+             //TODO: LISTAR ALUNOS   
+            break;
+        case "3":
+            //TODO: CALCULAR MEDIA GERAL  
+            break;
+        default:
+            throw new ArgumentOutOfRangeException();
+
+
+
+    
+}   
+    Console.WriteLine("Informe a opção desejada:");
+    Console.WriteLine("1- Inserir novo aluno");
+    Console.WriteLine("2- Listar alunos");
+    Console.WriteLine("3- Calcular média geral");
+    Console.WriteLine("X- Sair");
+    Console.WriteLine();
+
+    opcaoUsuario = Console.ReadLine();
+
+
+}
